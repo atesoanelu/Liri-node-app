e7d9a30c38a7988678598a1b14a980994bd4e74a
Ateso Anelu <atesofestus@gmail.com>
2017-09-28 23:09:02 -0700

    LIRI Bot

---
+++ keys.js
@@ -0,0 +1,11 @@
+console.log('this is loaded');
+
+var twitterKeys = {
+  consumer_key: 'OylrlcZC4aodLz5jgXwxUIIaW',
+  consumer_secret: 'VQMpXbo5qLQhLk8c4LFjpqVx3RXBDUAgfGI9BBU1SHxngoYOT5',
+  access_token_key: '912151920227131394-oAafjLPLH3uUhhIW9wmbMf6hU1uHx0I',
+  access_token_secret: 'h03vPQ1MR1jUJwHJpPW685Vft3j3Aq3ihieJZwEugxpPc',
+}
+
+module.exports = twitterKeys;
+
